44f7ddcf6525694d546337fab3c823609186317a
Fabio Esposito <fabio@tldv.io>
2022-07-10 18:28:52 +0200

    refactoring

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,7 @@ export class AppService {
   apiKey = process.env.INSTATUS_APIKEY || 'missing-api-key';
   apiConfig = {
     headers: {
-      //'Content-type': 'application/json',
+      'Content-type': 'application/json',
       Authorization: `Bearer ${this.apiKey}`,
     },
   };
